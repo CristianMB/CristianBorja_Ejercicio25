ebf625eb4b3e83f8ad110fb228d40170730ce818
Cristian Borja <cm.borja10@uniandes.edu.co>
2018-11-07 07:55:49 -0500

    Add files via upload

--- random_walk.py
+++ random_walk.py
@@ -1,12 +1,13 @@
+import matplotlib
+matplotlib.use('Agg')
 import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib import rc
 
 #Cristian Borja Ejercicio 25
 
 rc("text", usetex = True)
 resultados = np.loadtxt("random_walks.txt")
-fig, ax = plt.subplots()
+fig, ax = matplotlib.pyplot.subplots()
 ax.hist(resultados, normed = 1, bins = 100)
 ax.set_xlabel(r"Posici\'on final")
 ax.set_ylabel(r"Probabilidad")
